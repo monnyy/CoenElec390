3f3c627dbba07c81a6b7edc7f79f7794f13caf9d
Nick <35607155+npollender@users.noreply.github.com>
2018-03-28 19:53:17 -0400

    Fixed notifications not triggering
    Added check for API level.

--- medminder/app/src/main/java/com/example/nick/medminder/reminder/ReminderAlarmService.java
+++ medminder/app/src/main/java/com/example/nick/medminder/reminder/ReminderAlarmService.java
@@ -2,16 +2,19 @@ package com.example.nick.medminder.reminder;
 
 import android.app.IntentService;
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
 
 import com.example.nick.medminder.AddReminderActivity;
+import com.example.nick.medminder.LoginActivity;
 import com.example.nick.medminder.R;
 import com.example.nick.medminder.data.AlarmReminderContract;
 
@@ -21,8 +24,9 @@ import com.example.nick.medminder.data.AlarmReminderContract;
 
 public class ReminderAlarmService extends IntentService {
     private static final String TAG = ReminderAlarmService.class.getSimpleName();
-
-    private static final int NOTIFICATION_ID = 42;
+    private static final String id = "nick_channel_1";
+    private static final String name = "nick_1";
+;    private static final int NOTIFICATION_ID = 42;
     //This is a deep link intent, and needs the task stack
     public static PendingIntent getReminderPendingIntent(Context context, Uri uri) {
         Intent action = new Intent(context, ReminderAlarmService.class);
@@ -38,6 +42,8 @@ public class ReminderAlarmService extends IntentService {
     protected void onHandleIntent(Intent intent) {
 
         NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        NotificationCompat.Builder builder;
+        PendingIntent pendingIntent;
         Uri uri = intent.getData();
 
         //Display a notification to view the task details
@@ -61,18 +67,55 @@ public class ReminderAlarmService extends IntentService {
             }
         }
 
-        Notification note = new NotificationCompat.Builder(this)
-                .setContentTitle(getString(R.string.reminder_title))
-                .setContentText(description)
-                .setSmallIcon(R.drawable.ic_add_alert_black_24dp)
-                .setContentIntent(operation)
-                .setContentText("Time to take your medication!")  // required
-                .setVibrate(new long[] { 1000, 1000, 1000, 1000, 1000 })
-                .setSound(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.sound))
-                .setAutoCancel(true)
-                .setTicker("redMed Me!")
-                .build();
+        if (manager == null) {
+            manager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        }
 
-        manager.notify(NOTIFICATION_ID, note);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = manager.getNotificationChannel(id);
+            if (mChannel == null) {
+                mChannel = new NotificationChannel(id, name, importance);
+                mChannel.setDescription(description);
+                mChannel.enableVibration(true);
+                mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+                manager.createNotificationChannel(mChannel);
+            }
+            builder = new NotificationCompat.Builder(this, id);
+
+            intent = new Intent(this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+            builder.setContentTitle(getString(R.string.reminder_title))  // required
+                    .setSmallIcon(android.R.drawable.ic_popup_reminder) // required
+                    .setContentText(description)  // required
+                    .setDefaults(Notification.DEFAULT_ALL)
+                    .setAutoCancel(true)
+                    .setContentIntent(pendingIntent)
+                    .setTicker("redMed Me!")
+                    .setVibrate(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+        } else {
+
+            builder = new NotificationCompat.Builder(this);
+
+            intent = new Intent(this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+            builder.setContentTitle(getString(R.string.reminder_title))                           // required
+                    .setSmallIcon(android.R.drawable.ic_popup_reminder) // required
+                    .setContentText(description)  // required
+                    .setDefaults(Notification.DEFAULT_ALL)
+                    .setAutoCancel(true)
+                    .setContentIntent(pendingIntent)
+                    .setTicker("redMed Me!")
+                    .setVibrate(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400})
+                    .setPriority(Notification.PRIORITY_HIGH);
+        } // else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+        Notification notification = builder.build();
+        manager.notify(NOTIFICATION_ID, notification);
     }
 }
